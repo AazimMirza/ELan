ccf8e281a0788c49887dc1643fb2d99c5f6dec39
Aazim <137603432+AazimMirza@users.noreply.github.com>
2025-07-14 16:56:46 +0100

    Add files via upload

---
+++ aesth.js
@@ -0,0 +1,7 @@
+<script>
+  const text = ' • FEMALE FOUNDED • NATURAL RESULTS • BOUTIQUE APPROACH • ANTI-AGING ';
+  const textSlider = document.getElementById('textSlider');
+
+  let repeatText = text.repeat(50); // repeat enough times to make it long
+  textSlider.innerText = repeatText;
+</script>
